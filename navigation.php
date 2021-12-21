a3418f337d7a9af1369f242532ceacc3ad683a3b
Ilya <yowazza1@gmail.com>
2021-12-21 16:13:08 +0200

    added register,login,navigation

---
+++ navigation.php
@@ -0,0 +1,15 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+        <ul class="navigation">
+                <a href=""><li>На Главную</li></a>
+                <a href=""><li>Про Нас</li></a>
+                <a href=""><li>Связь С Нами</li></a>
+        </ul>
+</body>
+</html>
---
+++ register.php
@@ -0,0 +1,43 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+<div class="container">   
+    <div class="title">
+        <h2>Registration</h2>
+    </div>
+
+    <form method="post">
+        <div class="input-group">
+            <label>Username</label>
+            <input type="text" name="username" value="">
+        </div>
+        <div class="input-group">
+            <label>Email</label>
+            <input type="email" name="email" value="">
+        </div>
+        <div class="input-group">
+            <label>Password</label>
+            <input type="password" name="password_1">
+        </div>
+        <div class="input-group">
+            <label>Confirm Password</label>
+            <input type="password" name="password_2">
+        </div>
+        <div class="input-group">
+            <button type="submit" class="btn" name="reg_user">Register</button>
+        </div>
+        <div class="signin">
+        <p>
+            Alredy a member? <a href="">Sign in</a>
+        </p>
+        </div>
+    </form>
+</div>     
+</body>
+</html>
