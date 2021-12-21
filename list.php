5a253b2c0c55837e4f482c332c11f98fcfe23a34
Ilya <yowazza1@gmail.com>
2021-12-21 14:04:17 +0200

    added list and css

---
+++ list.php
@@ -0,0 +1,39 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+<div class="main">
+    <div class="article">
+        <h2>Blog 1</h2>
+    </div>
+        <div class="date">
+            <h3>Date - 10.12.2021</h3>
+        </div>
+            <div class="post">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Tempora nemo, perferendis iure cupiditate fuga consequuntur
+                necessitatibus eum aliquam impedit odit nobis ipsam, minima
+                accusamus. At autem eveniet illum minus enim?
+                </p>
+            </div>
+    <div class="article">
+        <h2>Blog 1</h2>
+    </div>
+        <div class="date">
+            <h3>Date - 10.12.2021</h3>
+        </div>
+            <div class="post">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Tempora nemo, perferendis iure cupiditate fuga consequuntur
+                necessitatibus eum aliquam impedit odit nobis ipsam, minima
+                accusamus. At autem eveniet illum minus enim?
+                </p>
+            </div>
+</div>
+</body>
+</html>
