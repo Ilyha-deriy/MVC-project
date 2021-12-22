98dd71ff93d758e8153a39f74ace47c0200b1064
Ilya <yowazza1@gmail.com>
2021-12-22 11:21:55 +0200

    Modified layout.php and list.php

--- layout.php
+++ layout.php
@@ -1,69 +1,58 @@
-
+<?php require_once "list.php" ?>
 <div class="main">
     <div class="article">
-        <h2>Blog 1</h2>
+        <h2><?php echo $blog1["title"] ?></h2>
     </div>
         <div class="date">
-            <h3>Date - 10.12.2021</h3>
+            <h3>Date - <?php echo $blog1["date"] ?></h3>
         </div>
             <div class="post">
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Tempora nemo, perferendis iure cupiditate fuga consequuntur
-                necessitatibus eum aliquam impedit odit nobis ipsam, minima
-                accusamus. At autem eveniet illum minus enim?
-                </p>
-            </div>
-    <div class="article">
-        <h2>Blog 1</h2>
-    </div>
-        <div class="date">
-            <h3>Date - 10.12.2021</h3>
-        </div>
-            <div class="post">
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Tempora nemo, perferendis iure cupiditate fuga consequuntur
-                necessitatibus eum aliquam impedit odit nobis ipsam, minima
-                accusamus. At autem eveniet illum minus enim?
+                <p>
+                    <?php echo $blog1["post"] ?>
                 </p>
             </div>
             <div class="article">
-        <h2>Blog 1</h2>
+        <h2><?php echo $blog2["title"] ?></h2>
     </div>
         <div class="date">
-            <h3>Date - 10.12.2021</h3>
+            <h3>Date - <?php echo $blog2["date"] ?></h3>
         </div>
             <div class="post">
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Tempora nemo, perferendis iure cupiditate fuga consequuntur
-                necessitatibus eum aliquam impedit odit nobis ipsam, minima
-                accusamus. At autem eveniet illum minus enim?
+                <p>
+                    <?php echo $blog2["post"] ?>
                 </p>
             </div>
             <div class="article">
-        <h2>Blog 1</h2>
+        <h2><?php echo $blog3["title"] ?></h2>
     </div>
         <div class="date">
-            <h3>Date - 10.12.2021</h3>
+            <h3>Date - <?php echo $blog3["date"] ?></h3>
         </div>
             <div class="post">
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Tempora nemo, perferendis iure cupiditate fuga consequuntur
-                necessitatibus eum aliquam impedit odit nobis ipsam, minima
-                accusamus. At autem eveniet illum minus enim?
+                <p>
+                    <?php echo $blog3["post"] ?>
                 </p>
             </div>
             <div class="article">
-        <h2>Blog 1</h2>
+        <h2><?php echo $blog4["title"] ?></h2>
     </div>
         <div class="date">
-            <h3>Date - 10.12.2021</h3>
+            <h3>Date - <?php echo $blog4["date"] ?></h3>
         </div>
             <div class="post">
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Tempora nemo, perferendis iure cupiditate fuga consequuntur
-                necessitatibus eum aliquam impedit odit nobis ipsam, minima
-                accusamus. At autem eveniet illum minus enim?
+                <p>
+                    <?php echo $blog4["post"] ?>
                 </p>
             </div>
-
+            <div class="article">
+        <h2><?php echo $blog5["title"] ?></h2>
+    </div>
+        <div class="date">
+            <h3>Date - <?php echo $blog5["date"] ?></h3>
+        </div>
+            <div class="post">
+                <p>
+                    <?php echo $blog5["post"] ?>
+                </p>
+            </div>
 </div>
--- list.php
+++ list.php
@@ -1,39 +1,31 @@
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="css/style.css">
-</head>
-<body>
-<div class="main">
-    <div class="article">
-        <h2>Blog 1</h2>
-    </div>
-        <div class="date">
-            <h3>Date - 10.12.2021</h3>
-        </div>
-            <div class="post">
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Tempora nemo, perferendis iure cupiditate fuga consequuntur
-                necessitatibus eum aliquam impedit odit nobis ipsam, minima
-                accusamus. At autem eveniet illum minus enim?
-                </p>
-            </div>
-    <div class="article">
-        <h2>Blog 1</h2>
-    </div>
-        <div class="date">
-            <h3>Date - 10.12.2021</h3>
-        </div>
-            <div class="post">
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Tempora nemo, perferendis iure cupiditate fuga consequuntur
-                necessitatibus eum aliquam impedit odit nobis ipsam, minima
-                accusamus. At autem eveniet illum minus enim?
-                </p>
-            </div>
-</div>
-</body>
-</html>
+<?php 
+
+$blog1 = array("title" => "Blog 1", "date" => "18.12.2021",
+"post" => "Lorem ipsum dolor sit amet consectetur adipisicing elit.
+Tempora nemo, perferendis iure cupiditate fuga consequuntur
+necessitatibus eum aliquam impedit odit nobis ipsam, minima
+accusamus. At autem eveniet illum minus enim?");
+
+$blog2 = array("title" => "Blog 2", "date" => "20.12.2021",
+"post" => "Lorem ipsum dolor sit amet consectetur adipisicing elit.
+Tempora nemo, perferendis iure cupiditate fuga consequuntur
+necessitatibus eum aliquam impedit odit nobis ipsam, minima
+accusamus. At autem eveniet illum minus enim?");
+
+$blog3 = array("title" => "Blog 3", "date" => "21.12.2021",
+"post" => "Lorem ipsum dolor sit amet consectetur adipisicing elit.
+Tempora nemo, perferendis iure cupiditate fuga consequuntur
+necessitatibus eum aliquam impedit odit nobis ipsam, minima
+accusamus. At autem eveniet illum minus enim?");
+
+$blog4 = array("title" => "Blog 4", "date" => "22.12.2021",
+"post" => "Lorem ipsum dolor sit amet consectetur adipisicing elit.
+Tempora nemo, perferendis iure cupiditate fuga consequuntur
+necessitatibus eum aliquam impedit odit nobis ipsam, minima
+accusamus. At autem eveniet illum minus enim?");
+
+$blog5 = array("title" => "Blog 5", "date" => "25.12.2021",
+"post" => "Lorem ipsum dolor sit amet consectetur adipisicing elit.
+Tempora nemo, perferendis iure cupiditate fuga consequuntur
+necessitatibus eum aliquam impedit odit nobis ipsam, minima
+accusamus. At autem eveniet illum minus enim?");
